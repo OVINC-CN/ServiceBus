54d1b6fb2d0cf2fffba5a969cd45e26956f71d90
Oren Zhang <orenzhang@ovinc.cn>
2022-12-13 14:28:46 +0800

    minor: ssl support for dev

--- entry/settings.py
+++ entry/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "sslserver",
     "apps.account",
     "apps.application",
     "apps.cel",
